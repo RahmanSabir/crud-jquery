68ae8d5b177bd25ba4980379ff74637ac9cee093
sabir <rahmansabir7@gmail.com>
2023-02-26 19:11:23 +0600

    rest api add

---
+++ src/main/java/com/example/loginCrudAjaxMysql/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.loginCrudAjaxMysql.controller;
+
+import com.example.loginCrudAjaxMysql.model.Employee;
+import com.example.loginCrudAjaxMysql.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> findAllUsers() {
+        return employeeRepository.findAll();
+    }
+}
